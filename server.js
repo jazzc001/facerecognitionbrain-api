88c98ab02d5e176bc5aeeeaf7e86db201c4ac8e7
jazzc001 <jazzcheung212@gmail.com>
2021-11-17 01:49:00 +0000

    adding port

--- server.js
+++ server.js
@@ -23,33 +23,7 @@ const db = knex({
 const app = express();
 
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '213',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-    login: [
-        {
-            od: '987',
-            has: '',
-            email: 'john@gmail.com'
-        }
-    ]
-}
+
 
 app.use(express.json());
 app.use(cors());
